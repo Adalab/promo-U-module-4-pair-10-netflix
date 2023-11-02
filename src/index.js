7b4be624108b9bca39fc20213dd87b29a6dc8b79
raquel <rachel050479@gmail.com>
2023-11-02 11:21:49 +0100

    added freedb_pairNetflix bd

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ const mysql = require('mysql2/promise');
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json({limit:'25mb'}));
+server.use(express.json({ limit: '25mb' }));
 
 // conexión a la base de datos
-async function getConnection(){
+async function getConnection() {
   const connection = await mysql.createConnection({
-      host:"localhost",
-      user:"root",
-      password:"d1234",
-      database: "netflix" 
+    host: 'sql.freedb.tech',
+    user: 'freedb_pairAdmin',
+    password: '3@3V@qU322p$?fJ',
+    database: 'freedb_pairNetflix',
   });
 
   connection.connect;
@@ -26,25 +26,26 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get('/movies', async (req, res)=>{ // (req, res): Require: para cuando envíen datos | Response: enviar desde el server datos al front
+server.get('/movies', async (req, res) => {
+  // (req, res): Require: para cuando envíen datos | Response: enviar desde el server datos al front
 
-    // obtener los datos de la bbdd
-    // 1.- obtener la conexión
-    const conn = await getConnection();
-    // 2.- consulta de la bbdd: obtener todas las alumnas
-    const queryMovies = "SELECT * FROM movies";
-    // 3.- ejecutar la consulta
-    const [results] = await conn.query(queryMovies); 
-    // [results] <- con [] porque de todo lo que trae, dame solo los resultados
-    console.log(results);
-    /* res.json(results); */
+  // obtener los datos de la bbdd
+  // 1.- obtener la conexión
+  const conn = await getConnection();
+  // 2.- consulta de la bbdd: obtener todas las alumnas
+  const queryMovies = 'SELECT * FROM movies';
+  // 3.- ejecutar la consulta
+  const [results] = await conn.query(queryMovies);
+  // [results] <- con [] porque de todo lo que trae, dame solo los resultados
+  console.log(results);
+  /* res.json(results); */
 
-    res.json({
+  res.json({
     success: true,
-    movies: results
-  }) 
-}) 
+    movies: results,
+  });
+});
 
 //servidor de estáticos
-const pathServerStatic = "./public_html";
+const pathServerStatic = './public_html';
 server.use(express.static(pathServerStatic));
